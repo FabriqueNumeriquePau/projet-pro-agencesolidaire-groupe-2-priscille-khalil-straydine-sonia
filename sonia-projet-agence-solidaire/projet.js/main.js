093afb3f596704f03e89b36d63c7400055da6865
lamine <soniamokaddem88@gmail.com>
2021-02-02 16:32:31 +0100

    avancement projet

---
+++ sonia-projet-agence-solidaire/projet.js/main.js
@@ -0,0 +1,17 @@
+let coll = document.querySelectorAll(".coll");
+let i;
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.visibility === "hidden" || content.style.visibility === "" )
+         {
+            content.style.visibility = "visible"; 
+        
+        }  else {
+            content.style.visibility = "hidden";
+            
+        }
+
+    });
+};
